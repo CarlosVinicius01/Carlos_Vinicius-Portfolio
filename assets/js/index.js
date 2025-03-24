7a467196250da8393e41aada62845a1c787dbd3f
c4rlosVinicius <pppcarlos8@gmail.com>
2025-03-24 11:47:47 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,11 @@ import seeMore from './seemore.js';
 import carousel from './carousel.js';
 import detail from './detail.js';
 import typewriter from './typewriter.js';
+import links from './links.js';
 
 
 seeMore();
 carousel();
 detail();
 typewriter();
+links();
