26148322db35046ef70c3430c88de0851c667f4c
c4rlosVinicius <pppcarlos8@gmail.com>
2025-04-06 20:37:58 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,6 @@ import detail from './detail.js';
 import typewriter from './typewriter.js';
 import links from './links.js';
 import menu from './menu.js';
-import form from './form.js';
 import contact from './contact.js';
 import disableZoom from './disableZoom.js';
 import theme from './theme.js'
@@ -15,7 +14,6 @@ detail();
 typewriter();
 links();
 menu();
-form();
 contact();
 disableZoom();
 theme();
